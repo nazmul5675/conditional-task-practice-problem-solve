6ef1c2219a72ab69fa0968d006b3c26f46e7ec48
nazmul5675 <hridoyn61@gmail.com>
2024-09-03 14:48:25 +0600

    free drink problem solved

---
+++ Free-Drinks.js
@@ -0,0 +1,14 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+let cost = 499;
+
+if (cost >= 500) {
+    console.log("free cock");
+}
+else {
+    console.log("Coke: 30 tk");
+}
